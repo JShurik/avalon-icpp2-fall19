e197f4605ad85f7928173c517df8c63a448d9281
JShurik <47276653+JShurik@users.noreply.github.com>
2019-11-23 21:05:58 +0300

    Add files via upload

---
+++ charString.hpp
@@ -0,0 +1,12 @@
+#pragma once
+namespace ext
+{
+	struct charString
+	{
+		char symbol;
+		charString* nextSymbol;
+	};
+
+	void append(charString*& str, char symbol);
+	void print(charString* str);
+}
---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "charString.hpp"
+
+using namespace std;
+using namespace ext;
+
+int main()
+{
+	charString* s1 = new charString{ 'h' };
+	charString* s2 = new charString{ 'e' };
+	charString* s3 = new charString{ 'l' };
+
+	charString* ptr = nullptr;
+	ptr = s1;
+	s1->nextSymbol = s2;
+	s2->nextSymbol = s3;
+
+	append(ptr, 'l');
+	print(ptr);
+}
